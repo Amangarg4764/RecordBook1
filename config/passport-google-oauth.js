9ffcdc359d426d928379f085f37ef7865b44deeb
Aman garg <aman0944.cse19@chitkara.edu.in>
2023-03-14 23:06:53 +0530

    index pagee

--- config/passport-google-oauth.js
+++ config/passport-google-oauth.js
@@ -7,43 +7,31 @@ passport.use(
   new googleStrategy(
     {
       clientID:
-        process.env.GOOGLE_ID ||
         "324409582740-1vnl2bll0485m29c6tbf3oca1aamo8le.apps.googleusercontent.com",
-      clientSecret:
-        process.env.GOOGLE_SECRET || "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",
-      callbackURL:
-        process.env.GOOGLE_CALLBACK ||
-        "http://localhost:8000/auth/google/callback",
+      clientSecret: "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",
+      callbackURL: "http://localhost:8000/auth/google/callback",
     },
-    function (accessToken, refreshToken, profile, done) {
-      User.findOne({ email: profile.emails[0].value }).exec(function (
-        err,
-        user
-      ) {
-        if (err) {
-          console.log("error in google oauth");
-          return;
-        }
-        //console.log(accessToken,refreshToken);
-        if (user) {
-          return done(null, user);
-        } else {
-          User.create(
-            {
-              name: profile.displayName,
-              email: profile.emails[0].value,
-              password: crypto.randomBytes(20).toString("hex"),
-            },
-            function (err, data) {
-              if (err) {
-                console.log("error in adding data using google oauth");
-                return;
-              }
-              return done(null, data);
+    async function (accessToken, refreshToken, profile, done) {
+      let user = await User.findOne({ email: profile.emails[0].value });
+
+      if (user) {
+        return done(null, user);
+      } else {
+        User.create(
+          {
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            password: crypto.randomBytes(20).toString("hex"),
+          },
+          function (err, data) {
+            if (err) {
+              console.log("error in adding data using google oauth");
+              return;
             }
-          );
-        }
-      });
+            return done(null, data);
+          }
+        );
+      }
     }
   )
 );
@@ -52,15 +40,10 @@ passport.serializeUser(function (user, done) {
   return done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
+passport.deserializeUser(async function (id, done) {
   //de
-  User.findById(id, function (err, user) {
-    if (err) {
-      console.log("err");
-      return;
-    }
-    return done(null, user);
-  });
+  let user = await User.findById(id);
+  return done(null, user);
 });
 
 passport.checkAuthentication = function (req, res, next) {
--- index.js
+++ index.js
@@ -1,20 +1,70 @@
 const express = require("express");
 const port = process.env.PORT || 8000;
 const path = require("path");
+const cookieParse = require("cookie-parser");
+const passport = require("passport");
+const googleStrategy = require("./config/passport-google-oauth");
 
 const database = require("./config/mongoose");
-
+const Mongostore = require("connect-mongo");
+const session = require("express-session");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "sitesPages"));
 app.use("/assest", express.static(path.join(__dirname, "assests")));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParse());
+app.use(
+  session({
+    name: "login",
+    secret: process.env.SESSION_SECRET || "xyz",
+    saveUninitialized: false,
+    resave: false,
+    //max age is 1 day = 1000 * 60 * 60 *24
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    store: Mongostore.create({
+      mongoUrl:
+        process.env.MONGODB_URL || "mongodb://localhost:27017/RecordBook",
+      mongooseConnect: database,
+      autoRemove: "disable",
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
 
 //------------Login page----------------------------------------------------------------------------------
+//link on google button give
 
-app.get("/", async function (req, res) {
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+//get ans from google
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  function (req, res) {
+    return res.redirect("/home");
+  }
+);
+//if it true than go to home page or not then go to login page
+app.get("/", function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/home");
+  }
   return res.render("login");
 });
+
+app.get("/logout", async function (req, res) {
+  await req.logout(req.user, function (err) {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
 //-----------------------------------------------------------------------------------------------------------
 app.use("/", require("./router/index"));
 app.listen(port, function (err) {
