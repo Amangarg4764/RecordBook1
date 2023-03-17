d20cfcdd40193439eb6d9f95cc9a333f51faa0c0
Aman garg <aman0944.cse19@chitkara.edu.in>
2023-03-17 11:58:08 +0530

    delete user router

--- router/logins.js
+++ router/logins.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const VehicleList = require("../model/addVehicle");
+const PersonalList = require("../model/personal");
+const MonthsDetails = require("../model/monthsDetails");
+const OtherList = require("../model/otherThings");
+const journeyList = require("../model/addJourney");
+const OtherThisList = require("../model/other");
 const Userlist = require("../model/user");
+const PersonalMonth = require("../model/personalMonth");
+
 //------------Login page----------------------------------------------------------------------------------
 
 //if it true than go to home page or not then go to login page
@@ -53,10 +61,40 @@ router.post(
   }
 );
 
-router.get("/deleteUser", async function (req, res) {
-  console.log("user");
-  // let us = await Userlist.findByIdAndDelete(req.query.id);
-  return res.redirect("back");
-});
+router.get(
+  "/deleteUser",
+  passport.checkAuthentication,
+  async function (req, res) {
+    let usa = await Userlist.findByIdAndDelete(req.query.id);
+    for (r of usa.vlist) {
+      let data = await VehicleList.findByIdAndDelete(r._id);
+      for (i of data.months) {
+        //other list delete
+        let data = await OtherList.find({ mowner: i });
+        for (j of data) {
+          let c = await OtherThisList.deleteMany({ listowner: j.id });
+        }
+        let dedata = await OtherList.deleteMany({ mowner: i });
+        //jorney list delete
+        let jdata = await journeyList.find({ mowner: i });
+        for (j of jdata) {
+          let c = await OtherThisList.deleteMany({ listowner: j.id });
+        }
+        let deddata = await journeyList.deleteMany({ mowner: i });
+
+        let mdata = await MonthsDetails.findByIdAndDelete(i);
+      }
+    }
+    for (r of usa.olist) {
+      let data = await PersonalList.findById(r._id).populate("child").exec();
+      for (i of data.child) {
+        await OtherThisList.deleteMany({ listowner: i._id });
+      }
+      await PersonalMonth.deleteMany({ parent: r._id });
+      await PersonalList.findByIdAndDelete(r._id);
+    }
+    return res.redirect("back");
+  }
+);
 
 module.exports = router;
